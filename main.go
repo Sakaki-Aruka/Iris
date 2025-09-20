9e52ecde254fd67489980ee0c996e983f26d7600
Sakaki-Aruka <aruka.works@gmail.com>
2025-09-21 03:23:53 +0900

    add: Config dir init func

--- main.go
+++ main.go
@@ -5,12 +5,58 @@ import (
 	"Iris/internal/profile"
 	"fmt"
 	"os"
+	"path/filepath"
 )
 
 func main() {
+
+	if err := CreateConfigDir(); err != nil {
+		os.Exit(1)
+	}
+
+	if err := CreateProfileDir(); err != nil {
+		os.Exit(1)
+	}
+
 	cmd.Execute()
 	if err := profile.LoadExistProfiles(); err != nil {
 		fmt.Println("failed to load profiles")
 		os.Exit(1)
 	}
+}
+
+func CreateConfigDir() error {
+	confDir, err := os.UserConfigDir()
+	if err != nil {
+		fmt.Println(err)
+		return err
+	}
+
+	confDir = filepath.Join(confDir, "iris")
+	if _, err := os.Stat(confDir); err != nil { // exists check
+		// '~/.config/iris' not exists.
+		mkdErr := os.Mkdir(confDir, 0770)
+		if mkdErr != nil {
+			fmt.Printf("failed to create Iris config dir. (%s)\n", confDir)
+			return mkdErr
+		}
+	}
+	return nil
+}
+
+func CreateProfileDir() error {
+	confDir, err := profile.GetProfileDir()
+	if err != nil {
+		fmt.Println(err)
+		return err
+	}
+
+	if _, err := os.Stat(confDir); err != nil { // exists check
+		mkdErr := os.Mkdir(confDir, 0770)
+		if mkdErr != nil {
+			fmt.Printf("failed to create Iris profile config dir. (%s)\n", confDir)
+			return mkdErr
+		}
+	}
+	return nil
 }
