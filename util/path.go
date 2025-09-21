782cbbb91e9497a31fec92860e2064403ed48963
Sakaki-Aruka <aruka.works@gmail.com>
2025-09-22 01:04:27 +0900

    add: util

---
+++ util/path.go
@@ -0,0 +1,40 @@
+package util
+
+import (
+	"Iris/ipc"
+	"os"
+	"path/filepath"
+)
+
+func GetConfDir() (string, error) {
+	d, err := os.UserConfigDir()
+	if err != nil {
+		return "", err
+	}
+	d = filepath.Join(d, "iris")
+	return d, nil
+}
+
+func GetProfileDir() (string, error) {
+	d, err := GetConfDir()
+	if err != nil {
+		return "", err
+	}
+	return filepath.Join(d, "profile"), nil
+}
+
+func GetSystemDir() (string, error) {
+	d, err := GetConfDir()
+	if err != nil {
+		return "", err
+	}
+	return filepath.Join(d, "sys"), nil
+}
+
+func GetSocketPath() (string, error) {
+	d, err := GetSystemDir()
+	if err != nil {
+		return "", err
+	}
+	return filepath.Join(d, ipc.SocketName), nil
+}
